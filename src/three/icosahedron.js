cb83d07bd5646f82703c172864f17758f98090f7
Asif Hazrat <asifhazrat@gmail.com>
2019-01-31 11:20:30 -0600

    biiist material

--- src/three/icosahedron.js
+++ src/three/icosahedron.js
@@ -3,7 +3,8 @@ import * as THREE from 'three'
 function genIcosahedron(offsetX) {
     // var material = new THREE.MeshNormalMaterial()
     var geometry = new THREE.IcosahedronGeometry(5, 0)
-    var material = new THREE.MeshLambertMaterial( { color: 0x00ff00 } )
+    var material = new THREE.MeshPhongMaterial( { color: 0x00ff00 } )
+    // var material = new THREE.MeshLambertMaterial( { color: 0x00ff00, wireframe: true } )
     var mesh = new THREE.Mesh(geometry, material)
     mesh.position.x = offsetX
     return mesh

0180870c2e6919d15d8916d9bf5c1e9bcafd62ec
Asif Hazrat <asifhazrat@gmail.com>
2019-02-06 12:41:43 -0600

    flesh out Atom, clean up Canvas using Atom class methods

--- src/components/Atom.js
+++ src/components/Atom.js
@@ -1,10 +1,38 @@
+import * as THREE from 'three'
+
 class Atom {
-    constructor(name) {
-        this.name = name
+
+    constructor(offsetX, atomDeets) {
+
+        this.loader = new THREE.TextureLoader()
+        this.texture = this.loader.load('https://i.imgur.com/VqIRECw.png', t => t)
+        this.geometry = new THREE.IcosahedronGeometry(5, 0)
+        this.material = new THREE.MeshBasicMaterial({ map: this.texture })
+        this.mesh = new THREE.Mesh(this.geometry, this.material)
+
+        this.mesh.position.x = offsetX
+
+        this.name = atomDeets[0]
+        this.protons = atomDeets[1]
+        this.neutrons = atomDeets[2]
+        this.electrons = atomDeets[3]
+        this.orbitalFields = atomDeets[4]
+
     }
-    sayHi() {
-        console.log('jello world ' + this.name)
+
+    addToScene(scene) {
+        scene.add(this.mesh)
+        console.log('jello world')
     }
+
+    spinAtom(direction) {
+        // console.log('jello ' + direction)
+        if (direction === 'up') {
+            this.mesh.rotation.x += 0.02
+            this.mesh.rotation.y += 0.04
+        }
+    }
+
 }
 
 export default Atom
--- src/components/Canvas.js
+++ src/components/Canvas.js
@@ -8,7 +8,6 @@ import camera from '../three/camera'
 import gui from '../three/gui'
 import planeGrid from '../three/planeGrid'
 import lines from '../three/lines'
-import icosahedron from '../three/icosahedron'
 import light from '../three/light'
 
 class Canvas extends React.Component {
@@ -17,25 +16,23 @@ class Canvas extends React.Component {
 
     componentDidMount() {
 
-        var atom = new Atom('hydrogen')
-        atom.sayHi()
-
         this.refs.canvas.appendChild(renderer.domElement)
         var scene = new THREE.Scene()
 
-        const icosahedron1 = icosahedron(scene, -10)
-        const icosahedron2 = icosahedron(scene, 10)
+        var atom1 = new Atom(-10, ['hydrogen', 1, 1, 0, 'up', '1p'])
+        var atom2 = new Atom(10, ['hydrogen', 1, 1, 0, 'up', '1p'])
+        atom1.addToScene(scene)
+        atom2.addToScene(scene)
+
         scene.add(planeGrid)
         scene.add(lines)
         scene.add(light)
-        gui(camera, light, icosahedron1, icosahedron2)
+        gui(camera, light, atom1.mesh, atom2.mesh)
 
         function animate() {
             requestAnimationFrame(animate)
-            icosahedron1.rotation.x += 0.02
-            icosahedron1.rotation.y += 0.04
-            icosahedron2.rotation.x += 0.02
-            icosahedron2.rotation.y += 0.04
+            atom1.spinAtom('up')
+            atom2.spinAtom('up')
             renderer.render(scene, camera)
         }
         animate()
